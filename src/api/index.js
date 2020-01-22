4b88b8bafa82d3a9e811f6eb8a17ba5ff8172137
Andrew Kozlov <fromlightbeam@gmail.com>
2020-01-22 23:07:50 +0300

    change add prod url. Connect to firebase

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 
-const BACK = 'http://localhost:8080'
+const BACK = 'https://frozen-meadow-30374.herokuapp.com'
+// const BACK = 'http://localhost:8080'
 
 export const GET_FOLLOWING = (user) => `${BACK}/${user}/following`
 export const GET_FOLLOWERS = (user) => `${BACK}/${user}/followers`
