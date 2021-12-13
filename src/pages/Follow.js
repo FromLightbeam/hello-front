d3bb49e9825221fabd08f592d00f5b04270a99b6
light <light@m.com>
2021-12-13 22:42:35 +0300

    Add prettier

--- src/pages/Follow.js
+++ src/pages/Follow.js
@@ -1,82 +1,104 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import MainLayout from '../layouts/main';
-import { useParams } from 'react-router-dom'; 
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MainLayout from "../layouts/main";
+import { useParams } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
-import * as api from '../api';
-import { Divider } from '@material-ui/core';
-
+import * as api from "../api";
+import { Divider } from "@material-ui/core";
 
 function Follows({ url, header }) {
   // May be make single variable ?
   const [userDo, setUserDo] = useState([]);
   const [userUndo, setUserUndo] = useState([]);
-  const [timestamp, setTimeStamp] = useState('');
+  const [timestamp, setTimeStamp] = useState("");
 
   function getFollowData() {
-    axios.get(url).then(response => {
+    axios.get(url).then((response) => {
       if (response.data) {
         setUserDo(response.data.do || []);
         setUserUndo(response.data.undo || []);
-        setTimeStamp(response.data.timestamp || '');
+        setTimeStamp(response.data.timestamp || "");
       }
-    }) 
-  } 
+    });
+  }
 
   useEffect(() => {
     getFollowData();
   }, []);
 
   function save() {
-    axios.post(url, {
-      do: userDo,
-      undo: userUndo
-    }).then(() => getFollowData())
+    axios
+      .post(url, {
+        do: userDo,
+        undo: userUndo,
+      })
+      .then(() => getFollowData());
   }
 
   var time = new Date(timestamp);
   return (
-    <Paper elevation={3} className='follow__paper'>
-      <div className='follow__paper-header'>
+    <Paper elevation={3} className="follow__paper">
+      <div className="follow__paper-header">
         <h3>{header}</h3>
-        <Button variant='contained' onClick={save}>save</Button>
+        <Button variant="contained" onClick={save}>
+          save
+        </Button>
       </div>
-      <Divider/>
-      <div>    
-        {userDo && userDo.length
-          ? userDo.map(user => <div key={user}>+<a target='_blank' rel="noopener noreferrer" href={`https://www.instagram.com/${user}/`}> {user}</a></div>) 
-          : <i>...</i>}
-      </div>
-      <Divider/>
+      <Divider />
       <div>
-        {userUndo && userUndo.length 
-          ? userUndo.map(user => <div key={user}>-<a target='_blank' rel="noopener noreferrer" href={`https://www.instagram.com/${user}/`}> {user}</a></div>) 
-          : <i>...</i>}
+        {userDo && userDo.length ? (
+          userDo.map((user) => (
+            <div key={user}>
+              +
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://www.instagram.com/${user}/`}
+              >
+                {user}
+              </a>
+            </div>
+          ))
+        ) : (
+          <i>...</i>
+        )}
       </div>
-      <Divider/>
-      <span className='label'>{time.toUTCString()}</span>
+      <Divider />
+      <div>
+        {userUndo && userUndo.length ? (
+          userUndo.map((user) => (
+            <div key={user}>
+              -
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`https://www.instagram.com/${user}/`}
+              >
+                {user}
+              </a>
+            </div>
+          ))
+        ) : (
+          <i>...</i>
+        )}
+      </div>
+      <Divider />
+      <span className="label">{time.toUTCString()}</span>
     </Paper>
   );
 }
-
-
 
 function Info() {
   const { userId } = useParams();
 
   return (
-    <MainLayout name='Follow'>
+    <MainLayout name="Follow">
       <div className="follows__items">
-        <Follows 
-          header='Following'
-          url={api.GET_FOLLOWING(userId)}
-        />
-        <Follows 
-          header='Followers'
-          url={api.GET_FOLLOWERS(userId)}/>   
-      </div> 
+        <Follows header="Following" url={api.GET_FOLLOWING(userId)} />
+        <Follows header="Followers" url={api.GET_FOLLOWERS(userId)} />
+      </div>
     </MainLayout>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { useForm } from "react-hook-form";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    '& > *': {
+    "& > *": {
       margin: theme.spacing(0.5),
-      width: '25ch',
+      width: "25ch",
     },
   },
 }));
 
-
 function Login() {
   const classes = useStyles();
-  const { register, handleSubmit } = useForm(); 
+  const { register, handleSubmit } = useForm();
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
     <div className="login__form">
       <h1>Hello!</h1>
-      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
-        <TextField inputProps={{name: "username", ref: register}} label="Username" />
-        <TextField inputProps={{name: "password", ref: register}} label="Password" type="password" />
-        <input type="submit" style={{ display: "none"}}/>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <TextField
+          inputProps={{ name: "username", ref: register }}
+          label="Username"
+        />
+        <TextField
+          inputProps={{ name: "password", ref: register }}
+          label="Password"
+          type="password"
+        />
+        <input type="submit" style={{ display: "none" }} />
       </form>
     </div>
   );
